fdcc6a216fa65f889cefb6011e0c5ce3f8d61d1e
Tanawut-Wiriya <tanawut.wry@gmail.com>
2024-07-15 11:24:03 +0700

    2nd commit

---
+++ lib/ModbusSlave/examples/modbus.ino
@@ -0,0 +1,103 @@
+#include <Arduino.h>
+#include "modbus.h"
+#include "modbusDevice.h"
+#include "modbusRegBank.h"
+#include "modbusSlave.h"
+//#include <SoftwareSerial.h>  // Modbus RTU pins   D7(13),D8(15)   RX,TX
+
+modbusDevice regBank;
+modbusSlave slave;
+ 
+void setup()
+{   
+
+//Assign the modbus device ID.  
+  regBank.setId(1);
+
+
+// Holding registers
+
+  regBank.add(40001);  
+  regBank.add(40002);  
+  regBank.add(40003);  
+  regBank.add(40004);  
+  regBank.add(40005);  
+  regBank.add(40006);  
+  regBank.add(40007);  
+  regBank.add(40008);  
+  regBank.add(40009);  
+  regBank.add(40010);  
+  
+  regBank.add(40011);  
+  regBank.add(40012);  
+  regBank.add(40013);  
+  regBank.add(40014);  
+  regBank.add(40015);  
+  regBank.add(40016);  
+  regBank.add(40017);  
+  regBank.add(40018);  
+  regBank.add(40019);  
+  regBank.add(40020);  
+
+  slave._device = &regBank;  
+
+  Serial.begin(9600);
+  delay(100);
+// Initialize the serial port for coms at 9600 baud  
+  slave.setBaud(9600);
+  delay(100);
+  Serial.println("Connected "); 
+  Serial.println("Modbus RTU Slave Online");  
+}
+
+void loop()
+{    
+     int Mdelay = 5; // microdelay 
+
+     Serial.print("[0] ");
+     Serial.print(regBank.get(40000)); 
+     delay(Mdelay);
+     Serial.print(" [1] ");
+     Serial.print(regBank.get(40001));
+     delay(Mdelay);
+     Serial.print(" [2] ");
+     Serial.print(regBank.get(40002));
+     delay(Mdelay);
+     Serial.print(" [3] ");
+     Serial.print(regBank.get(40003));
+     delay(Mdelay);
+     Serial.print(" [4] ");
+     Serial.print(regBank.get(40004));
+     delay(Mdelay);
+     Serial.print(" [5] ");
+     Serial.print(regBank.get(40005));
+     delay(Mdelay);
+     Serial.print(" [6] ");
+     Serial.print(regBank.get(40006));
+     delay(Mdelay);
+     Serial.print(" [7] ");
+     Serial.print(regBank.get(40007));
+     delay(Mdelay);
+     Serial.print(" [8] ");
+     Serial.print(regBank.get(40008));        
+     delay(Mdelay);
+     Serial.print(" [9] ");
+     Serial.println(regBank.get(40009));        
+     delay(Mdelay);
+
+   ///////// Holding Register [10] A [19] = 10 Holding Registers Escritura
+   ///////// Holding Register [10] A [19] = 10 Holding Registers Writing
+
+    regBank.set(40010,  random(1, 100));        
+    regBank.set(40011,  random(1, 100));        
+    regBank.set(40012,  random(1, 100));              
+    regBank.set(40013,  random(1, 100));              
+    regBank.set(40014,  random(1, 100)); 
+    regBank.set(40015,  random(1, 100));        
+    regBank.set(40016,  random(1, 100));        
+    regBank.set(40017,  random(1, 100));              
+    regBank.set(40018,  random(1, 100));              
+    regBank.set(40019,  random(1, 100)); 
+    slave.run();  
+
+}
